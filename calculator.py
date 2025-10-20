dc431da065bc344d7af04ae4c9d352bf17c95858
van4ik <privdarov6@gmail.com>
2025-10-20 11:09:27 +0500

    Добавлена валидация ввода пользователя

--- calculator.py
+++ calculator.py
@@ -1,4 +1,10 @@
-#Простой калькулятор
+#Улучшенный калькулятор
+def get_number(prompt):
+	while True:
+		try:
+			return float(input(prompt))
+		except ValueError:
+			print("Ошибка: введите число")
 def add(a,b):
 	return a+b
 def subtract(a,b):
@@ -10,6 +16,10 @@ def divide(a,b):
 		return a/b
 	else:
 		return "Ошибка: деление на ноль"
+def power(a,b):
+	return a**b
+def square_root(a):
+	return a**0.5
 #Основная программа
-printf("Простой калькулятор")
-printf("Доступные операции:+,-,*,/")
+printf("Улучшенный калькулятор")
+printf("Доступные операции:+,-,*,/,^,sqrt")
